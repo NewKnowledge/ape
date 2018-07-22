793f9a9b811ea23c3a92263473214eeedc64121c
Craig Corcoran <craig@newknowledge.io>
2018-07-22 10:24:52 -0500

    add pytest to setup and versions to requirements.txt

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ setup(name='nk_ape',
       description='Abstractive Prediction via Embeddings',
       packages=['nk_ape'],
       install_requires=[
-          'numpy>=1.13.3',
           'gensim==3.2.0',
           'inflection>=0.3.1',
+          'numpy>=1.13.3',
+          'ontospy>=1.8.6',
           'pandas>=0.19.2',
-          'ontospy>=1.8.6'
+          'pytest>=3.6.2',
       ],
       include_package_data=True
       )
